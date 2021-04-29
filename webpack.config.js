5206b884bb6b9a5b8ec3611c09d06a03e3df751f
Alexander Winnen <alwinnen@gmail.com>
2021-04-29 18:07:13 +0200

    target es5

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 const path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  target: "es5",
   entry: path.resolve(__dirname, './src/index.js'),
   output: {
     filename: 'index.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+	library: 'Dryvue',
+	libraryTarget: 'umd'
   },
+  externals: { "vue": "vue" },
   module: {
     rules: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }]
   },
